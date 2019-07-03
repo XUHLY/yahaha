1eb9a5b08fe6ad0be241bf8a0b1b8c5a3084859c
huangliyang <840504522@qq.com>
2019-07-03 14:31:50 +0800

    yahaha......

---
+++ src/main/java/cn/yahaha/App.java
@@ -0,0 +1,13 @@
+package cn.yahaha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
